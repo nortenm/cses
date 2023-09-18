5b49ae97b2f27b43f70f2c54aaf7c5af70e83078
Nameet <143340218+nortenm@users.noreply.github.com>
2023-09-18 15:30:29 +0530

    Update appleDivTLE.cpp

--- introductory problems/appleDivTLE.cpp
+++ introductory problems/appleDivTLE.cpp
@@ -3,25 +3,6 @@
 using namespace std;
 int main()
 {
-  int t;
-  cin>>t;
-  while(t--)
-    {
-      int a,b;
-      cin>>a>>b;
-      while(a && b)
-        {
-          if(a>b)
-          {
-            a-=2;
-            b--;
-          }
-          else{
-            a--;
-            b-=2;
-          }
-        }
-      if(a==0 && b==0) cout<<"YES"<<endl;
-      else cout<<"NO"<<endl;
-    }
+  int n;
+  cin>>n;
 }
